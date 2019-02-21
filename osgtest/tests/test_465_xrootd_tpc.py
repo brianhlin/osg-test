eae2f184aede27646675f0a3904273d64c5faf44
Brian Lin <brianhlin@gmail.com>
2019-02-20 18:44:48 -0600

    let's do even less

--- osgtest/tests/test_465_xrootd_tpc.py
+++ osgtest/tests/test_465_xrootd_tpc.py
@@ -1,14 +1,7 @@
-import os
-import json
-import requests
-
-import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
 
 
-class TestXrootdTPC(osgunittest.OSGTestCase):
-
-    __data_path = '/usr/share/osg-test/test_gridftp_data.txt'
+class TestXrootdTpc(osgunittest.OSGTestCase):
 
     def test_01_create_macaroons(self):
         return True
