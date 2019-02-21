903029491e70d2b1e50f8c105f09ae80def1a756
Brian Lin <brianhlin@gmail.com>
2019-02-20 18:51:50 -0600

    Revert "let's do even less"
    This reverts commit eae2f184aede27646675f0a3904273d64c5faf44.

--- osgtest/tests/test_465_xrootd_tpc.py
+++ osgtest/tests/test_465_xrootd_tpc.py
@@ -1,7 +1,14 @@
+import os
+import json
+import requests
+
+import osgtest.library.core as core
 import osgtest.library.osgunittest as osgunittest
 
 
-class TestXrootdTpc(osgunittest.OSGTestCase):
+class TestXrootdTPC(osgunittest.OSGTestCase):
+
+    __data_path = '/usr/share/osg-test/test_gridftp_data.txt'
 
     def test_01_create_macaroons(self):
         return True
